6a9f71c440918b4a697924ca33b2c6b145cb60e1
caphuutuan <caphuutuan1@gmail.com>
2022-11-10 14:46:39 +0700

    Add project files.

---
+++ SnoopShop/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SnoopShop.Models;
+using System.Web.Mvc;
+using SnoopShop.Common;
+
+namespace SnoopShop.Areas.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        private SnoopShopDbContext db = new SnoopShopDbContext();
+        public ActionResult Login()
+        {
+            if (Session["Admin_Name"] != null)
+            {
+                Response.Redirect("~/Admin");
+            }
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Login(String User, String Pass)
+        {
+            int count_username = db.User.Where(m => m.Status == 1 && ((m.Phone).ToString() == User|| m.Email == User || m.Name == User) && m.Access != 1).Count();
+            if (count_username == 0)
+                return Json(new { s = 1 });
+            else
+            {
+                String Password = XString.ToMD5(Pass);
+                //String Password = Pass;
+                var user_acount = db.User
+                .Where(m => m.Status == 1 && ((m.Phone).ToString() == User || m.Email == User || m.Name == User) && m.Access != 1 && m.Password == Password);
+                if (user_acount.Count() == 0)
+                    return Json(new { s = 2 });
+                else
+                {
+                    var user = user_acount.First();
+                    Session["Admin_fullname"] = user.Fullname;
+                    Session["Admin_ID"] = user.ID;
+                    Session["Admin_Name"] = user.Name;
+                    Session["Admin_Images"] = user.Image;
+                    Session["Admin_Address"] = user.Address;
+                    Session["Admin_Phone"] = user.Phone;
+                    role role = db.Roles.Where(m => m.parentId == user.Access).First();
+                    var userSession = new Userlogin();
+                    userSession.UserName = user.Name;
+                    userSession.UserID = user.ID;
+                    userSession.GroupID = role.GropID;
+                    userSession.AccessName = role.accessName;
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                    var i = Session["SESSION_CREDENTIALS"];
+                    Session["Admin_ID"] = user.ID;
+                    Session["Admin_Name"] = user.Name;
+                    Session["Admin_fullname"] = user.Fullname;
+                    return Json(new { s = 0 });
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            if (Session["Admin_Name"] != null)
+            {
+                Session["Admin_Name"] = null;
+                Session["Admin_ID"] = null;
+            }
+            return Redirect("~/Admin/Login");
+        }
+    }
+}
